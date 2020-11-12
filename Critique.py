fb6d8ff9a1c4a31096cab4bd70d0d20a7896d9ab
casoarctik <58051216+casoarctik@users.noreply.github.com>
2020-11-12 09:57:22 +0100

    Mise à jour des fonctions inscriptions et connexion, ajout de la fonction déconnexion

--- Critique.py
+++ Critique.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import click
+import functools
 from flask.cli import with_appcontext
 from flask import(
      Flask, render_template, request,
@@ -9,7 +10,6 @@ from flask import(
      session, flash, g, current_app,
 
 )
-from markupsafe import escape
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
@@ -17,13 +17,56 @@ app.config.from_mapping(
     SECRET_KEY = 'dev'
 )
 
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            os.path.join(app.instance_path, 'schema.sql'),
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
 
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables"""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+app.teardown_appcontext(close_db)
+app.cli.add_command(init_db_command)
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if session.get('id') is None:
+            return redirect(url_for('index'))
+        return view(**kwargs)
+    return wrapped_view
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+     return render_template('index.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -64,45 +107,6 @@ def register():
 
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            os.path.join(app.instance_path, 'schema.sql'),
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
-
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables"""
-    init_db()
-    click.echo('Initialized the database.')
-
-
-app.teardown_appcontext(close_db)
-app.cli.add_command(init_db_command)
-
-
-# It's the route for our main page, the user have to connect
-# with his logs to get farther
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -125,4 +129,10 @@ def login():
             return redirect(url_for('index'))
 
         flash(error)
-    return render_template('index.html')
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
